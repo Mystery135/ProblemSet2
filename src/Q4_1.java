1243c12aa7ddac255525146f81af3ca253baf54a
Mystery135 <websiteemail12345678910@gmail.com>
2024-01-11 02:49:26 -0700

    Finished

--- src/Q4_1.java
+++ src/Q4_1.java
@@ -2,7 +2,7 @@ public class Q4_1 {
     public static void main(String[] args) {
         myMethod(0);
     }
-    public static void myMethod(int num){//*No base case*
+    public static void myMethod(int num){//Error: *No base case*
         System.out.println(num + " ");
         myMethod(num + 1);
     }
--- src/Q5.java
+++ src/Q5.java
@@ -3,18 +3,33 @@ import java.util.Arrays;
 public class Q5 {
     public static void main(String[] args) {
         System.out.println(maxElement(new int[]{1, 5, 1, 8, 9, 2, -9999}));
+        System.out.println(maxElement(new int[]{1, 5, 1, 8, 9, 2, -9999}, 0, 0));
     }
+
+
+    //With 1 parameter
     public static int maxElement(int[] ints){
         if (ints.length == 1){return ints[0];}
         if (ints[0] <= ints[1]){
             ints = Arrays.copyOfRange(ints, 1, ints.length);
-            System.out.println(Arrays.toString(ints));
         }else{
             int temp = ints[0];
             ints = Arrays.copyOfRange(ints, 1, ints.length);
             ints[0] = temp;
         }
         return maxElement(ints);
-
         }
+
+    //With 3 parameters
+    public static int maxElement(int[] ints, int from, int currentMax){
+        if (from == ints.length-1){
+            return currentMax;
+        }
+        if (ints[from] > currentMax){
+            currentMax = ints[from];
+        }
+        return maxElement(ints, from+1, currentMax);
+    }
+
+
 }
